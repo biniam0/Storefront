07c3da25957f2792e11557633a0d88f64a60f62e
bini <jaletakebede@gmail.com>
2024-10-30 02:28:24 -0900

    Managers and QuerySet

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from store.models import Product
 # Create your views here.
 
 
 def say_hello(request):
-    x = 1
-    y = 2
-    return render(request, 'hello.html', {'name': 'Bini'})
+    query_set = Product.objects.all()
+    # To evaluate QuerySet we need to:
+        # iterate over it
+            # for product in query_set:
+            #     print(product)
+        # convert it to a list
+            # list(query_set)
+        # access individual element or slice it
+            # query_set[0:5]
+    
+    return render(request, 'hello.html', {'name': rows_in_table})
